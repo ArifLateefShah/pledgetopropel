316d82bfd5cf4b4886c40af82b03f34f67d407ff
ShahArif <arif.shah22@gmail.com>
2023-02-16 21:18:13 +0530

    main

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Peledge to Propel</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  .bg-primary{
+    background-image: url('./top-banner.png');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 20%;
+  }
+
+  .banner-content-left{
+    color: black;
+    border-radius: 5px;
+  }
+  .bg-dark{
+    background-color: black;
+  }
+  footer{
+    background-color: black;
+    color: #fff;
+  }
+  .footer-left{
+    float: left;
+  }
+  .footer-right{
+    float: right;
+  }
+  </style>
+</head>
+<body>
+    <div class="main">
+        <div class="p-5 bg-primary">
+            <div class="container">
+                <div class="banner">
+                    <div class="logo">
+                        <img src="./logo.png">
+                    </div>
+                    <div class="nav-links">
+                        <!-- <h4>Connect</h4> -->
+                    </div>
+                </div>
+                <div class="row content mt-5">
+                    <div class="col-md-6 col-sm-12 bg-white p-5 banner-content-left">
+                        <h1>When one door closes, another opens</h1>
+                        <p class="pt-3">
+                            Pledge2Propel is on a mission to help technology professionals who've lost their jobs find new opportunities for growth and success.
+                       </p>
+                       <p>
+                            After all, adversity is just an opportunity to rise again, more intelligently and with renewed purpose.
+                        </p>
+                        <p>
+                            Our goal is to empower you with all the resources you need, because with the right guidance there is no limit to what you can achieve.
+                        </p>
+                    </div>
+                    <div class="col-md-6">
+    
+                    </div>
+                </div>
+            </div> 
+        </div>
+        <footer class="p-4 pb-5">
+            <p class="footer-left">Copyright © Ideas2IT | All Rights Reserved</p>
+            <p class="footer-right">Privacy Policy | Terms of Use</p>
+        </footer>
+    </div>
+</body>
+</html>
